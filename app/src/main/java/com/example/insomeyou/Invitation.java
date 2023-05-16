3b81dfbe33696f4b57419d2d1d45631ee5e7d22b
andreimorie <andreimorie@gmail.com>
2023-05-16 22:50:14 +0800

    changing font styles :I

--- app/src/main/java/com/example/insomeyou/Invitation.java
+++ app/src/main/java/com/example/insomeyou/Invitation.java
@@ -2,14 +2,25 @@ package com.example.insomeyou;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Invitation extends AppCompatActivity {
 
+    Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_invitation);
 
+        button = findViewById(R.id.buttonLogin);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Invitation.this, LoginActivity.class));
+            }
+        });
     }
 }
