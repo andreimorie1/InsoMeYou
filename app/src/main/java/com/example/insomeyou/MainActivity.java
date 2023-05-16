d281924195d5a10b37513d8c0efe1b5fc2abc6ed
andreimorie <andreimorie@gmail.com>
2023-05-16 20:48:33 +0800

    awye

--- app/src/main/java/com/example/insomeyou/MainActivity.java
+++ app/src/main/java/com/example/insomeyou/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.insomeyou;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -9,16 +10,20 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    CardView cardView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        cardView = findViewById(R.id.cardView1);
+        cardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, IdeasTab.class));
+            }
+        });
+    }
 
-    }
-    public void informationactivity(){
-        Intent intent = new Intent(MainActivity.this, IdeasTab.class);
-        startActivity(intent);
-    }
 
 }
